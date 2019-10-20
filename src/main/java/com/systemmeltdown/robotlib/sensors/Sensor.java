cfdbb1a855bbe5e82475c2ceb4a82c2862c00f17
Kevin Killingsworth <kevin@redfenix.com>
2019-10-20 16:45:26 -0500

    Add base classes for Sensor and LimitSensor
    This adds base classes for any kind of sensor and for any kind of a
    Limit Sensor.

---
+++ src/main/java/com/systemmeltdown/robotlib/sensors/Sensor.java
@@ -0,0 +1,12 @@
+package com.systemmeltdown.robotlib.sensors;
+
+import edu.wpi.first.wpilibj.SendableBase;
+
+/**
+ * Base Sensor Class
+ * 
+ * This is the base class for all kinds of sensors
+ */
+
+public abstract class Sensor extends SendableBase {
+}
