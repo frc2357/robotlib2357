60c746ced6e121d70f37d4cc46df4e0f1ee5fb64
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2019-10-27 16:33:14 -0500

    Remove rememnets of testing WPIlib's code

--- src/test/java/com/systemmeltdown/robotlib/triggers/ToggleTriggerTest.java
+++ src/test/java/com/systemmeltdown/robotlib/triggers/ToggleTriggerTest.java
@@ -4,14 +4,10 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.command.Command;
 
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
 public class ToggleTriggerTest {
 	@Test
@@ -43,37 +39,4 @@ public class ToggleTriggerTest {
 
 		trigger.close();
 	}
-
-  // TODO: create test for if command if started on trigger activation
-	// @Test
-	// public void testToggleTriggersCommand() {
-  //   ShuffleboardTab tab = Shuffleboard.getTab("Test");
-  //   NetworkTableEntry entry = tab.add("Toggle Triggers Command", false)
-  //     .withWidget(BuiltInWidgets.kToggleButton)
-  //     .getEntry();
-
-  //   Command testCommand = mock( Command.class );
-  //   doAnswer(new Answer<Void>() {
-  //     @Override
-  //     public Void answer(InvocationOnMock invocation) throws Throwable {
-  //       System.out.println("answer test");
-  //       return null;
-  //     }
-  //   }).when( testCommand ).start();
-
-    
-  //   ToggleTrigger trigger = new ToggleTrigger( entry );
-  //   trigger.whenActive(testCommand);
-
-  //   // changing the entry value, but doesn't change triggers active
-    
-  //   System.out.println(trigger.get());
-  //   System.out.println("IsCompleted " + testCommand.isCompleted());
-  //   System.out.println("IsCanceled " + testCommand.isCanceled());
-  //   System.out.println("IsRunning " + testCommand.isRunning());
-  //   assertEquals( testCommand.isCompleted(), true );
-    
-  //   testCommand.cancel();
-	// 	trigger.close();
-  // }
 }
