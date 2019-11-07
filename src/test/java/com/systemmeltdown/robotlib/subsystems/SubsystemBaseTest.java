d1f40ec5831b21e25945142d93853a9d68b1a3b1
NC2357 <53975849+NC2357@users.noreply.github.com>
2019-11-07 15:49:23 -0600

    Update/test subsystem base (#10)
    * Tests the SubsystemBase to see if fail safe is active.
    
    * Tests whether the failsafe is active or not active.

---
+++ src/test/java/com/systemmeltdown/robotlib/subsystems/SubsystemBaseTest.java
@@ -0,0 +1,46 @@
+package com.systemmeltdown.robotlib.subsystems;
+
+import com.systemmeltdown.robotlib.subsystems.SubsystemBase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class SubsystemBaseTest {
+	@Test
+	public void testSetFailsafeActive() {
+		SubsystemBase subBase = new TestSubsystem();
+
+		subBase.setFailsafeActive(true);
+    
+		assertEquals(subBase.isFailsafeActive(), true );
+
+		subBase.close();
+	}
+
+	@Test
+	public void testSetFailsafeNotActive() {
+		SubsystemBase subBase = new TestSubsystem();
+
+		subBase.setFailsafeActive(false);
+    
+		assertEquals(subBase.isFailsafeActive(), false );
+
+		subBase.close();
+	}
+
+	@Test
+	public void testIsFailsafeActive() {
+        SubsystemBase subBase = mock(SubsystemBase.class);
+        
+		assertEquals( subBase.isFailsafeActive(), false );
+
+		subBase.close();
+	}
+
+	private class TestSubsystem extends SubsystemBase{
+		@Override
+		public void initDefaultCommand(){
+		}
+	}
+}
+
