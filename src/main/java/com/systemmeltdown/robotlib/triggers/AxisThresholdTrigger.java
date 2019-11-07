6ae9be96d535e7ac2ef504f416f38b4e7d2da674
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2019-11-07 16:12:23 -0600

    Rename DriverSlowTrigger to AxisThresholdTrigger, add hand to contructor, add more tests
    Further generalize by renaming and letting the user set the hand to use, and add a test for if the trigger is close to the threshold or right on it.

--- src/main/java/com/systemmeltdown/robotlib/triggers/AxisThresholdTrigger.java
+++ src/main/java/com/systemmeltdown/robotlib/triggers/AxisThresholdTrigger.java
@@ -4,22 +4,20 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.buttons.Trigger;
 
-public class DriverSlowTrigger extends Trigger {
+public class AxisThresholdTrigger extends Trigger {
   private XboxController controller;
+  private Hand hand;
   private double triggerThreshold;
 
-  public DriverSlowTrigger(XboxController controller) {
-    this(controller, 0.25);
-  }
-
-  public DriverSlowTrigger(XboxController controller, double triggerThreshold) {
+  public AxisThresholdTrigger(XboxController controller, Hand hand, double triggerThreshold) {
     this.controller = controller;
+    this.hand = hand;
     this.triggerThreshold = triggerThreshold;
   }
 
   @Override
   public boolean get() {
-    double axis = controller.getTriggerAxis(Hand.kRight);
+    double axis = controller.getTriggerAxis(hand);
 
     return (axis > triggerThreshold);
   }
