480ab018b7dfac5dc8c3395a05f2e4eda96c97e6
CrazyFloob <crazyfloob@gmail.com>
2019-11-14 16:57:27 -0600

    Fixed indention issue.

--- src/test/java/com/systemmeltdown/robotlib/controllers/GunnerControlsTest.java
+++ src/test/java/com/systemmeltdown/robotlib/controllers/GunnerControlsTest.java
@@ -7,135 +7,135 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class GunnerControlsTest{
-    @Test
-    public void testGetProportionalTurnIsOneWhenRightStickIsOneAndTurnRateIsOne(){
-        XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetProportionalTurnIsOneWhenRightStickIsOneAndTurnRateIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-        when(controller.getX(Hand.kRight)).thenReturn(1.0);
+    when(controller.getX(Hand.kRight)).thenReturn(1.0);
 
-        GunnerControls gunnerControlTester = new GunnerControls(controller,1.0,1.0,0.1,0,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,1.0,1.0,0.1,0,0);
 
-        assertEquals(gunnerControlTester.getProportionalTurn(), 0.9,1.0);
-    }
+    assertEquals(gunnerControlTester.getProportionalTurn(), 0.9,1.0);
+  }
 
-    @Test
-    public void testGetProportionalTurnIsNegativeOneWhenRightStickIsNegativeOneAndTurnRateIsOne(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetProportionalTurnIsNegativeOneWhenRightStickIsNegativeOneAndTurnRateIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getX(Hand.kRight)).thenReturn(-1.0);
+    when(controller.getX(Hand.kRight)).thenReturn(-1.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,1.0,1.0,0.1,0,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,1.0,1.0,0.1,0,0);
 
-      assertEquals(gunnerControlTester.getProportionalTurn(), -1.0,-0.9);
-    }
+    assertEquals(gunnerControlTester.getProportionalTurn(), -1.0,-0.9);
+  }
 
-    @Test
-    public void testGetProportionalTurnIsZeroWhenRightStickIsZeroAndTurnRateIsOneHalf(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetProportionalTurnIsZeroWhenRightStickIsZeroAndTurnRateIsOneHalf(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getX(Hand.kRight)).thenReturn(0.0);
+    when(controller.getX(Hand.kRight)).thenReturn(0.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,0.5,1.0,0.1,0,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.5,1.0,0.1,0,0);
 
-      assertEquals(gunnerControlTester.getProportionalTurn(), 0.0,0.0);
-    }
+    assertEquals(gunnerControlTester.getProportionalTurn(), 0.0,0.0);
+  }
 
-    @Test
-    public void testGetProportionalSpeedIsNegativeOneWhenLeftStickIsOneAndSpeedFactorIsOne(){
-        XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetProportionalSpeedIsNegativeOneWhenLeftStickIsOneAndSpeedFactorIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-        when(controller.getY(Hand.kLeft)).thenReturn(1.0);
+    when(controller.getY(Hand.kLeft)).thenReturn(1.0);
 
-        GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,1.0,0.1,0,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,1.0,0.1,0,0);
 
-        assertEquals(gunnerControlTester.getProportionalSpeed(), -1.0,-0.9);
-    }
+    assertEquals(gunnerControlTester.getProportionalSpeed(), -1.0,-0.9);
+  }
 
-    @Test
-    public void testGetProportionalSpeedIsOneWhenLeftStickIsNegativeOneAndSpeedFactorIsOne(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetProportionalSpeedIsOneWhenLeftStickIsNegativeOneAndSpeedFactorIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getY(Hand.kLeft)).thenReturn(-1.0);
+    when(controller.getY(Hand.kLeft)).thenReturn(-1.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,1.0,0.1,0,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,1.0,0.1,0,0);
 
-      assertEquals(gunnerControlTester.getProportionalSpeed(), 0.9,1.0);
-    }
+    assertEquals(gunnerControlTester.getProportionalSpeed(), 0.9,1.0);
+  }
 
-    @Test
-    public void testGetProportionalSpeedIsZeroLeftStickIsZeroAndSpeedFactorIsOneHalf(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetProportionalSpeedIsZeroLeftStickIsZeroAndSpeedFactorIsOneHalf(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getY(Hand.kLeft)).thenReturn(0.0);
+    when(controller.getY(Hand.kLeft)).thenReturn(0.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.5,0.1,0,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.5,0.1,0,0);
 
-      assertEquals(gunnerControlTester.getProportionalSpeed(), 0.0,0.0);
-    }
+    assertEquals(gunnerControlTester.getProportionalSpeed(), 0.0,0.0);
+  }
 
-    @Test
-    public void testGetEncoderTurnDifferentialIsOneWhenRightStickIsOneAndGunnerEncoderTurnRateIsOne(){
-        XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetEncoderTurnDifferentialIsOneWhenRightStickIsOneAndGunnerEncoderTurnRateIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-        when(controller.getX(Hand.kRight)).thenReturn(1.0);
+    when(controller.getX(Hand.kRight)).thenReturn(1.0);
 
-        GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,0,1);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,0,1);
 
-        assertEquals(gunnerControlTester.getEncoderTurnDifferential(), 0.9,1.0);
-    }
+    assertEquals(gunnerControlTester.getEncoderTurnDifferential(), 0.9,1.0);
+  }
 
-    @Test
-    public void testGetEncoderTurnDifferentialIsNegativeOneWhenRightStickIsNegativeOneAndGunnerEncoderTurnRateIsOne(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetEncoderTurnDifferentialIsNegativeOneWhenRightStickIsNegativeOneAndGunnerEncoderTurnRateIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getX(Hand.kRight)).thenReturn(-1.0);
+    when(controller.getX(Hand.kRight)).thenReturn(-1.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,0,1);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,0,1);
 
-      assertEquals(gunnerControlTester.getEncoderTurnDifferential(), -1.0,-0.9);
-    }
+    assertEquals(gunnerControlTester.getEncoderTurnDifferential(), -1.0,-0.9);
+  }
 
-    @Test
-    public void testGetEncoderTurnDifferentialIsZeroWhenRightStickIsZeroAndGunnerEncoderTurnRateIsTwo(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetEncoderTurnDifferentialIsZeroWhenRightStickIsZeroAndGunnerEncoderTurnRateIsTwo(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getX(Hand.kRight)).thenReturn(0.0);
+    when(controller.getX(Hand.kRight)).thenReturn(0.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,0,2);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,0,2);
 
-      assertEquals(gunnerControlTester.getEncoderTurnDifferential(), 0.0,0.0);
-    }
+    assertEquals(gunnerControlTester.getEncoderTurnDifferential(), 0.0,0.0);
+  }
 
-    @Test
-    public void testGetEncoderSpeedIsNegativeOneWhenLeftStickIsOneAndGunnerEncoderSpeedIsOne(){
-        XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetEncoderSpeedIsNegativeOneWhenLeftStickIsOneAndGunnerEncoderSpeedIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-        when(controller.getY(Hand.kLeft)).thenReturn(1.0);
+    when(controller.getY(Hand.kLeft)).thenReturn(1.0);
 
-        GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,1,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,1,0);
 
-        assertEquals(gunnerControlTester.getEncoderSpeed(), -1.0,-0.9);
-    }
+    assertEquals(gunnerControlTester.getEncoderSpeed(), -1.0,-0.9);
+  }
 
-    @Test
-    public void testGetEncoderSpeedIsOneWhenLeftStickIsNegativeOneAndGunnerEncoderSpeedIsOne(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetEncoderSpeedIsOneWhenLeftStickIsNegativeOneAndGunnerEncoderSpeedIsOne(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getY(Hand.kLeft)).thenReturn(-1.0);
+    when(controller.getY(Hand.kLeft)).thenReturn(-1.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,1,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.0,0.1,1,0);
 
-      assertEquals(gunnerControlTester.getEncoderSpeed(), 0.9,1.0);
-    }
+    assertEquals(gunnerControlTester.getEncoderSpeed(), 0.9,1.0);
+  }
 
-    @Test
-    public void testGetEncoderSpeedIsZeroLeftStickIsZeroAndSpeedFactorIsTwo(){
-      XboxController controller = mock(XboxController.class);
+  @Test
+  public void testGetEncoderSpeedIsZeroLeftStickIsZeroAndSpeedFactorIsTwo(){
+    XboxController controller = mock(XboxController.class);
 
-      when(controller.getY(Hand.kLeft)).thenReturn(0.0);
+    when(controller.getY(Hand.kLeft)).thenReturn(0.0);
 
-      GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.5,0.1,2,0);
+    GunnerControls gunnerControlTester = new GunnerControls(controller,0.0,0.5,0.1,2,0);
 
-      assertEquals(gunnerControlTester.getEncoderSpeed(), 0.0,0.0);
-    }
+    assertEquals(gunnerControlTester.getEncoderSpeed(), 0.0,0.0);
+  }
 }
