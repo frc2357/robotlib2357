a30fe12961ad07f49c727bc0e1822dd9287b452a
NC2357 <53975849+NC2357@users.noreply.github.com>
2019-11-24 14:11:14 -0600

    Completed testing functionality of Utility.java

--- src/test/java/com/systemmeltdown/robotlib/util/UtilityTest.java
+++ src/test/java/com/systemmeltdown/robotlib/util/UtilityTest.java
@@ -4,6 +4,51 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UtilityTest{
+
+    //double clamp tests
+
+    @Test
+    public void testDoubleClampReturnsThreeAndTwoTenthsWhenMinIsNegativeFiveAndAHalfAndMaxIsFiveAndAHalfAndInputIsThreeAndTwoTenths(){
+        assertEquals(Utility.clamp(3.2,-5.5,5.5), 3.2, 0.0);
+    }
+
+    @Test
+    public void testDoubleClampReturnsNegativeFiveAndAHalfWhenMinIsNegativeFiveAndAHalfAndMaxIsFiveAndAHalfAndInputIsNegativeSevenAndAHalf(){
+        assertEquals(Utility.clamp(-7.5,-5.5,5.5), -5.5, 0.0);
+    }
+
+    @Test
+    public void testDoubleClampReturnsFiveAndAHalfWhenMinIsNegativeFiveAndAHalfAndMaxIsFiveAndAHalfAndInputIsEightAndTwoTenths(){
+        assertEquals(Utility.clamp(8.2,-5.5,5.5), 5.5, 0.0);
+    }
+
+    //int clamp tests
+
+    @Test
+    public void testIntClampReturnsThreeWhenMinIsNegativeFiveAndMaxIsFiveAndInputIsThree(){
+        assertEquals(Utility.clamp(3,-5,5), 3, 0);
+    }
+
+    @Test
+    public void testIntClampReturnsNegativeFiveWhenMinIsNegativeFiveAndMaxIsFiveAndInputIsNegativeSeven(){
+        assertEquals(Utility.clamp(-7,-5,5), -5, 0);
+    }
+
+    @Test
+    public void testIntClampReturnsFiveWhenMinIsNegativeFiveAndMaxIsFiveAndInputIsEight(){
+        assertEquals(Utility.clamp(8,-5,5), 5, 0);
+    }
+
+    //getAverage tests
+    @Test
+    public void testGetAverageReturnsFiveWithTenElementArrayFromOneToTen(){
+        int[] samples = {1,2,3,4,5,6,7,8,9,10};
+
+        assertEquals(Utility.getAverage(samples), 5, 0);
+    }
+
+    //Deadband Tests
+
     @Test
     public void testDeadbandReturnsOneWhenInputIsOneAndDeadbandIsOneHalf(){
         assertEquals(Utility.deadband(1.0,0.5), 0.9, 1.0);
