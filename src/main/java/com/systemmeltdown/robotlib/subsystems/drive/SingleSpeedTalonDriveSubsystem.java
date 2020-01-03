356a9c0d9cc99752937531940caca04a6e6d1b7a
Eli Monnig <35583627+Stick-2357@users.noreply.github.com>
2020-01-02 18:12:18 -0600

    fix drivesub constructor

--- src/main/java/com/systemmeltdown/robotlib/subsystems/drive/SingleSpeedTalonDriveSubsystem.java
+++ src/main/java/com/systemmeltdown/robotlib/subsystems/drive/SingleSpeedTalonDriveSubsystem.java
@@ -10,21 +10,29 @@ public class SingleSpeedTalonDriveSubsystem extends SkidSteerDriveSubsystem {
     private WPI_TalonSRX[] m_rightSlaves;
     private WPI_TalonSRX[] m_leftSlaves;
 
-    public SingleSpeedTalonDriveSubsystem(int rightMasterId, int leftMasterId, int[] rightSlaveIds,
-            int[] leftSlaveIds) {
+    public SingleSpeedTalonDriveSubsystem(
+        int rightMasterId, 
+        int leftMasterId, 
+        int[] rightSlaveIds,
+        int[] leftSlaveIds,
+        boolean invertRight,
+        boolean invertLeft) {
 
         // init right master
         m_rightMaster = new WPI_TalonSRX(rightMasterId);
         m_rightMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        m_rightMaster.setInverted(invertRight);
 
         // init left master
         m_leftMaster = new WPI_TalonSRX(leftMasterId);
         m_leftMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        m_leftMaster.setInverted(invertLeft);
 
         // init right slaves
         m_rightSlaves = new WPI_TalonSRX[rightSlaveIds.length];
         for (int i = 0; i < m_rightSlaves.length; i++) {
             m_rightSlaves[i] = new WPI_TalonSRX(rightSlaveIds[i]);
+            m_rightSlaves[i].setInverted(invertRight);
             m_rightSlaves[i].follow(m_rightMaster);
         }
 
@@ -32,6 +40,7 @@ public class SingleSpeedTalonDriveSubsystem extends SkidSteerDriveSubsystem {
         m_leftSlaves = new WPI_TalonSRX[leftSlaveIds.length];
         for (int i = 0; i < m_leftSlaves.length; i++) {
             m_leftSlaves[i] = new WPI_TalonSRX(leftSlaveIds[i]);
+            m_leftSlaves[i].setInverted(invertLeft);
             m_leftSlaves[i].follow(m_leftMaster);
         }
 
