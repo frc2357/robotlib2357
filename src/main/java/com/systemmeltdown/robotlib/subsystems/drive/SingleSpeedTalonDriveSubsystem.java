2b528dc874298e1fde8bf23a08dc8462673bc22b
Stick-2357 <35583627+Stick-2357@users.noreply.github.com>
2020-01-03 17:03:32 -0600

    Add TalonGroup, move drivesub constructor to use TalonGroup

--- src/main/java/com/systemmeltdown/robotlib/subsystems/drive/SingleSpeedTalonDriveSubsystem.java
+++ src/main/java/com/systemmeltdown/robotlib/subsystems/drive/SingleSpeedTalonDriveSubsystem.java
@@ -1,62 +1,40 @@
 package com.systemmeltdown.robotlib.subsystems.drive;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class SingleSpeedTalonDriveSubsystem extends SkidSteerDriveSubsystem {
-    private WPI_TalonSRX m_rightMaster;
-    private WPI_TalonSRX m_leftMaster;
-    private WPI_TalonSRX[] m_rightSlaves;
-    private WPI_TalonSRX[] m_leftSlaves;
+    private TalonGroup m_rightTalonGroup;
+    private TalonGroup m_leftTalonGroup;
 
     public SingleSpeedTalonDriveSubsystem(
-        TalonIDGroup rightTalonIDs,
-        TalonIDGroup leftTalonIDs) {
-
-        // init right master
-        m_rightMaster = new WPI_TalonSRX(rightTalonIDs.getMasterTalonID());
-        m_rightMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
-        m_rightMaster.setInverted(rightTalonIDs.getInvert());
-
-        // init left master
-        m_leftMaster = new WPI_TalonSRX(leftTalonIDs.getMasterTalonID());
-        m_leftMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
-        m_leftMaster.setInverted(leftTalonIDs.getInvert());
-
-        // init right slaves
-        m_rightSlaves = new WPI_TalonSRX[rightTalonIDs.getSlaveTalonIDs().length];
-        for (int i = 0; i < m_rightSlaves.length; i++) {
-            m_rightSlaves[i] = new WPI_TalonSRX(rightTalonIDs.getSlaveTalonIDs()[i]);
-            m_rightSlaves[i].setInverted(rightTalonIDs.getInvert());
-            m_rightSlaves[i].follow(m_rightMaster);
-        }
-
-        // init left slaves
-        m_leftSlaves = new WPI_TalonSRX[leftTalonIDs.getSlaveTalonIDs().length];
-        for (int i = 0; i < m_leftSlaves.length; i++) {
-            m_leftSlaves[i] = new WPI_TalonSRX(leftTalonIDs.getSlaveTalonIDs()[i]);
-            m_leftSlaves[i].setInverted(leftTalonIDs.getInvert());
-            m_leftSlaves[i].follow(m_leftMaster);
-        }
-
-        // TODO: add more config to talons?
+        TalonGroup rightTalonGroup,
+        TalonGroup leftTalonGroup) {
+            m_rightTalonGroup = rightTalonGroup;
+            m_leftTalonGroup = leftTalonGroup;
     }
 
     @Override
     protected int getCurrentSpeedLeftClicksPerSecond() {
-        return m_leftMaster.getSelectedSensorPosition();
+        // could add a getSelectedSensorPosition command to the TalonGroup, so that
+        // we don't have to call getMasterTalon
+        return m_leftTalonGroup.getMasterTalon().getSelectedSensorPosition();
     }
 
     @Override
     protected int getCurrentSpeedRightClicksPerSecond() {
-        return m_rightMaster.getSelectedSensorPosition();
+        // could add a getSelectedSensorPosition command to the TalonGroup, so that
+        // we don't have to call getMasterTalon
+        return m_rightTalonGroup.getMasterTalon().getSelectedSensorPosition();
     }
 
     @Override
     protected void setProportional(double leftProportion, double rightProportion) {
-        m_leftMaster.set(ControlMode.PercentOutput, leftProportion);
-        m_rightMaster.set(ControlMode.PercentOutput, rightProportion);
+        // could add a set command to the TalonGroup, so that
+        // we don't have to call getMasterTalon
+        m_leftTalonGroup.getMasterTalon().set(ControlMode.PercentOutput, leftProportion);
+        // could add a set command to the TalonGroup, so that
+        // we don't have to call getMasterTalon
+        m_rightTalonGroup.getMasterTalon().set(ControlMode.PercentOutput, rightProportion);
     }
 
     @Override
