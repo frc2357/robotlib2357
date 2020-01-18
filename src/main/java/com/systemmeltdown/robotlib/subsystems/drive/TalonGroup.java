4992ff7b8bdf0658aca0ca45052573916634b9c7
Stick-2357 <35583627+Stick-2357@users.noreply.github.com>
2020-01-18 09:28:47 -0600

    fix talongroup constructor

--- src/main/java/com/systemmeltdown/robotlib/subsystems/drive/TalonGroup.java
+++ src/main/java/com/systemmeltdown/robotlib/subsystems/drive/TalonGroup.java
@@ -10,8 +10,9 @@ public class TalonGroup {
 
     public TalonGroup(int masterTalonID, int[] slaveTalonIDs) {
         m_masterTalon = new WPI_TalonSRX(masterTalonID);
+        m_slaveTalons = new WPI_TalonSRX[slaveTalonIDs.length];
 
-        for (int i = 0; i <= slaveTalonIDs.length; i++) {
+        for (int i = 0; i < slaveTalonIDs.length; i++) {
             m_slaveTalons[i] = new WPI_TalonSRX(slaveTalonIDs[i]);
             m_slaveTalons[i].follow(m_masterTalon);
         }
