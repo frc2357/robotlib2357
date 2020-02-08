46a15a098da7ef51b9ee5d79b0a504b1ef05e0fe
GameMagma <53975931+GameMagma@users.noreply.github.com>
2020-02-08 12:50:13 -0600

    Added capability to get VL530LOX sensor data.

--- src/main/java/com/systemmeltdown/robotlib/sensors/VL53LOXSensorOutput.java
+++ src/main/java/com/systemmeltdown/robotlib/sensors/VL53LOXSensorOutput.java
@@ -3,6 +3,10 @@ package com.systemmeltdown.robotlib.sensors;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 
+import java.io.FileReader;
+
+import org.json.simple.JSONObject;
+
 public class VL53LOXSensorOutput {
     private SerialPort m_serialPort;
 
@@ -10,9 +14,17 @@ public class VL53LOXSensorOutput {
         m_serialPort = new SerialPort(115200, port);
     }
 
-    public String read() {
+    public String getJSONString() {
         int count = m_serialPort.getBytesReceived();
-
+        
         return m_serialPort.readString(count);
+    }
+
+    public int getInt(String value) {
+        int count = m_serialPort.getBytesReceived();
+        JSONObject sensorInfo = (JSONObject) new JSONParser()
+            .parse(new FileReader(m_serialPort.readString(count));
+
+        return sensorInfo.get(value);
     }
 }
