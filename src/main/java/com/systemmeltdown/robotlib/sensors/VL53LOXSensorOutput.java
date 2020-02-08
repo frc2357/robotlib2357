733393029cdf784958455429247019422d57cc4a
NC2357 <crazyfloob@gmail.com>
2020-02-08 15:51:13 -0600

    Added subsystem to test intake TOF's

--- src/main/java/com/systemmeltdown/robotlib/sensors/VL53LOXSensorOutput.java
+++ src/main/java/com/systemmeltdown/robotlib/sensors/VL53LOXSensorOutput.java
@@ -3,9 +3,15 @@ package com.systemmeltdown.robotlib.sensors;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class VL53LOXSensorOutput {
     private SerialPort m_serialPort;
@@ -16,15 +22,30 @@ public class VL53LOXSensorOutput {
 
     public String getJSONString() {
         int count = m_serialPort.getBytesReceived();
-        
+
         return m_serialPort.readString(count);
     }
 
-    public int getInt(String value) {
+    public void getInt(String value) throws FileNotFoundException, IOException, ParseException {
         int count = m_serialPort.getBytesReceived();
-        JSONObject sensorInfo = (JSONObject) new JSONParser()
-            .parse(new FileReader(m_serialPort.readString(count));
+        String info = m_serialPort.readString(count);
+        System.out.println(info);
+        Object obj = new JSONParser()
+            .parse(info);
+        JSONObject sensorInfo = (JSONObject) obj;
 
-        return sensorInfo.get(value);
+        Map address = ((Map)sensorInfo.get(value));
+
+        //System.out.println(sensorInfo.isEmpty());
+
+        Iterator<Map.Entry> itr1 = address.entrySet().iterator(); 
+        while (itr1.hasNext()) { 
+            Map.Entry pair = itr1.next(); 
+            System.out.println(pair.getKey() + " : " + pair.getValue()); 
+        } 
+
+        count = (int) sensorInfo.get(value);
+        //return count;
     }
 }
+//Map address = ((Map)jo.get("address")); 
