5d2f169e9d0c2f6d59de0bf168b0b8e1ce9a7e89
coderkevin <kevin@redfenix.com>
2022-12-11 15:55:58 -0800

    Simplify SensorDevice settings further

--- arduino/libraries/Sensor2357/src/SensorDevice.h
+++ arduino/libraries/Sensor2357/src/SensorDevice.h
@@ -64,17 +64,14 @@ public:
 protected:
   SensorDevice(const char *deviceName, Print &out)
     : m_out(out),
-      m_maxUpdateMsJson(m_settingsJson[0]),
+      m_maxUpdateMsJson(m_fieldsJson[1]),
       m_jsonState(m_sensorDeviceJson)
   {
     m_lastUpdateMs = 0;
     
-    // Initalize device settings JSON
-    m_maxUpdateMsJson = Json::Int("maxUpdateMs", SENSOR_DEVICE_DEFAULT_MAX_UPDATE_MS);
-
     // Create sensor device JSON object
     m_fieldsJson[0] = Json::String("name", deviceName);
-    m_fieldsJson[1] = Json::Object("settings", m_settingsJson);
+    m_fieldsJson[1] = Json::Int("maxUpdateMs", SENSOR_DEVICE_DEFAULT_MAX_UPDATE_MS);
     m_fieldsJson[2] = Json::Object("sensors", m_sensorsJson);
     m_sensorDeviceJson = Json::Object(m_fieldsJson);
   }
@@ -92,7 +89,6 @@ protected:
 private:
   Print &m_out;
   JsonElement &m_maxUpdateMsJson;
-  JsonElement m_settingsJson[1];
   JsonElement m_fieldsJson[3];
   JsonElement m_sensorsJson[S];
   void *m_sensorFuncs[S];
