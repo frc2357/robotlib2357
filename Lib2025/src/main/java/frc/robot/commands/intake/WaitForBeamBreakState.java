e20c0a37b6b64f762e2ce754e0ca3c82d0add2dc
Kenneth430 <treygethers@gmail.com>
2024-11-26 18:44:23 -0600

    Changed "WaitForBeamBreak" to "WaitForBeamBreakState"

--- Lib2025/src/main/java/frc/robot/commands/intake/WaitForBeamBreakState.java
+++ Lib2025/src/main/java/frc/robot/commands/intake/WaitForBeamBreakState.java
@@ -3,7 +3,7 @@ package frc.robot.commands.intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 
-public class WaitForBeamBreak extends Command {
+public class WaitForBeamBreakState extends Command {
   @Override
   public boolean isFinished() {
     return Robot.intake.isBeamBroken();
